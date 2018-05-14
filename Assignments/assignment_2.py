e01c8025ab1edc1a634058104c052949cfedba0b
Iván Hernández Ortega <ivan@wupplier.com>
2018-05-14 20:10:45 +0100

    Minor improvement in assignment 2

--- Assignments/assignment_2.py
+++ Assignments/assignment_2.py
@@ -19,7 +19,7 @@ def print_section_title(title_message):
     print('-'*30)
 
 
-def print_names(conditions):
+def print_names(conditions=[]):
     if CONDITION_LONGER_THAN_5_CHARACTERS in conditions:
         if CONDITION_CONTAINS_N in conditions:
             for name in names:
@@ -35,8 +35,8 @@ def print_names(conditions):
 
 
 # 1) Create a list of names and use a for loop to output the length of each name (len() ).
-print_section_title('Printing all names')
-print_names([])
+print_section_title('Printing all names (' + str(len(names))+')')
+print_names()
 
 
 # 2) Add an if  check inside the loop to only output names longer than 5 characters.
@@ -45,7 +45,7 @@ print_names([CONDITION_LONGER_THAN_5_CHARACTERS])
 
 # 3) Add another if  check to see whether a name includes a “n”  or “N”  character.
 print_section_title(
-    'Printing only names longer than 5 characters AND contains an N or n')
+    "Printing only names longer than 5 characters AND includes a 'N' or 'n'")
 print_names([CONDITION_LONGER_THAN_5_CHARACTERS, CONDITION_CONTAINS_N])
 # 4) Use a while  loop to empty the list of names (via pop() )
 print_section_title('Clearing names list')
